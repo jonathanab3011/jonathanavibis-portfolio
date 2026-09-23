ad040a4f09585baffd1b396777ef9a346d7e1eac
jonathanavibiswas10 <joanthan.avibd24@gmail.com>
2026-09-23 15:14:56 +0600

    Fix import paths in page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,19 @@
-import Navbar from "@/components/Navbar";
-import Home from "@/components/Home";
-import AboutSection from "@/components/AboutSection";
-import ProjectsSection from "@/components/ProjectsSection";
-import ContactSection from "@/components/ContactSection";
-import Footer from "@/components/Footer";
+import Navbar from "@/app/component/navbar";
+import Home from "@/app/component/home";
+import AboutSection from "@/app/component/about/about";
+import ProjectsSection from "@/app/component/projects/projects";
+import ContactSection from "@/app/component/Contract/contract";
+import Footer from "@/app/component/footer";
 
 export default function Page() {
   return (
-    <>
+    <main>
       <Navbar />
       <Home />
       <AboutSection />
       <ProjectsSection />
       <ContactSection />
       <Footer />
-    </>
+    </main>
   );
 }
